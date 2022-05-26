1b81dd5c7b1e85feb26364783e49cf06d8e18441
Luana Brenna Negreiros Lucas <102502197+luanalucas@users.noreply.github.com>
2022-05-26 14:03:16 -0300

    Update 1010.c

--- 1010.c
+++ 1010.c
@@ -2,25 +2,22 @@
 
 int cod_prod;
 int quant;
-double valor;
+float valor;
 
 int cod_prod2;
 int quant2;
-double valor2;
-
-void comprar(int cod_prod, int quant, double valor, int cod_prod2, int quant2, double valor2){
-    if(quant>=1 && quant2>=1){
-        
-        double soma= (quant*valor) + (quant2*valor2);
-        printf("\nValor a pagar: %f", soma);
-           
-    }
-}
+float valor2;
 
 int main() {
-    comprar(12, 1, 5.30, 16, 2, 5.10);
-    comprar(13, 2, 15.30, 161, 4, 5.20);
-    comprar(1, 1, 15.10, 2, 1, 15.10);
     
+  printf("\nEntre com o valor 1: ");
+  scanf("%d %d %f", &cod_prod, &quant, &valor);
+  printf("\nEntre com o valor 2: ");
+  scanf("%d %d %f", &cod_prod2, &quant2, &valor2);
+
+   
+   float soma= (quant*valor) + (quant2*valor2);
+   printf("VALOR A PAGAR: R$ %.2f",soma);
+       
     return 0;
 }
