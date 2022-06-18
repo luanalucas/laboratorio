35477215cd256e83b4b48adc4e6f07e9e0e5fa31
Luana Brenna Negreiros Lucas <102502197+luanalucas@users.noreply.github.com>
2022-06-18 20:17:56 -0300

    Update trabalhobancatcc.c

--- trabalhobancatcc.c
+++ trabalhobancatcc.c
@@ -7,23 +7,29 @@ typedef struct Banca{
     int matricula;
     char *curso;
     char *email_aluno;
+
     //orientador nome
     char *orientador;
+
     //frequencia
     double frequencia;
+
     //dados avaliadores
     char *avaliadores;
     char *instituicao;
     char *email_avaliador;
+
     //dados da banca
     char *titulo_banca;
     char *resumo;
     double horario;
     char *local;
+    
     //dados aluno
     struct Banca * prox;
     struct Banca * ant;
 }BANCA;
+
 
 BANCA * inicio = NULL;
 BANCA * fim = NULL;
@@ -68,8 +74,7 @@ void salvar(){
     system("pause");
 }
 
-void carregarArquivo(file){
-
+/*void carregarArquivo(file){
     file = fopen ("arquivo.txt","rb"); //abrindo arquivo
         if(file == NULL){
                 printf("Erro na abertura!\n");
@@ -80,10 +85,7 @@ void carregarArquivo(file){
     fread(&cad,sizeof(struct BANCA),1,file);
     printf("%s\n%d\n%s\n%s\n%s\n%f\n%s\n%s\n%s\n%s\n%f\n%s\n", cad.aluno, cad.matricula, cad. curso, cad.curso, cad.email_aluno, cad.orientador, cad.frquencia, cad.avaliadores,
     cad.institucao, cad.email_avaliador, cad.titulo_banca, cad.resumo, cad.horario, cad.local);  
-    //Pegar o que ta no arquivo, read
-    //salvar na lista p sempre
-    //funcao atualizar dados
-}
+}*/
 
 
 void cadastrar(char *aluno, int matricula, char *curso, char *email_aluno, char *orientador, float frequencia, char *avaliadores,
@@ -208,12 +210,12 @@ void imprimir(){
     }
 }
 
-BANCA removerEntrada(){
+void remover(){ //Segmentation fault BANCA *remover()
     
     char * aluno = malloc(sizeof(aluno));
     int matricula;
 
-    printf("""\n------------------------REMOVER---------------------------------\n""");  
+    printf("\n------------------------REMOVER---------------------------------\n");  
 
     printf("\nDigite a matricula do aluno: ");
     scanf("%d", &matricula);
@@ -229,83 +231,149 @@ BANCA removerEntrada(){
         while(aux != fim && aux->matricula != matricula){
             aux = aux->prox;
         }
-        if(aux->matricula == matricula){
+
+        if(inicio->matricula == matricula){
+            inicio = inicio->prox;
+        }
+        else if(fim->matricula == matricula){
+            fim = fim->prox;
+        }
+        else if(aux->matricula == matricula){
             aux->ant->prox = aux->prox;
             aux->prox->ant = aux->ant;
-            if(inicio->matricula == matricula){
-                inicio = inicio->prox;
-            }
-            if(fim->matricula == matricula){
-                fim = fim->prox;
-            }
-            free(aux);
-            tam--;
-        }else{
-            printf("Não foi encontrado! \n");
         }
-   }
+        else{
+            printf("Não foi encontrado! \n");
+            return;
+        }
+        free(aux);
+        tam--;
+    }
+    printf("Banca removida com sucesso!");
 }
 
-BANCA* atualizar(){ 
+void atualizar(){ //Segmentation fault
     BANCA *aux = inicio;
-    char *alt;
-
-    char *aluno;
+    
+    int mat;
+    int opcao;
+    char *aluno = malloc(sizeof(aluno));
     int matricula;
-    char *curso;
-    char *email_aluno;//@
+    char *curso = malloc(sizeof(curso));
+    char *email_aluno = malloc(sizeof(email_aluno));
     //orientador nome
-    char *orientador;
+    char *orientador = malloc(sizeof(orientador));
     //frequencia
     double frequencia;
     //dados avaliadores
-    char *avaliadores;
-    char *instituicao;
-    char *email_avaliador;
+    char *avaliadores = malloc(sizeof(avaliadores));
+    char *instituicao = malloc(sizeof(instituicao));
+    char *email_avaliador = malloc(sizeof(email_avaliador));
     //dados da banca
-    char *titulo_banca;
-    char *resumo;
+    char *titulo_banca = malloc(sizeof(titulo_banca));
+    char *resumo = malloc(sizeof(resumo));
     double horario;
-    char *local;
+    char *local = malloc(sizeof(local));
 
-    printf("""\n------------------------ATUALIZAR---------------------------------\n""");  
+    printf("\n------------------------ATUALIZAR---------------------------------\n");  
 
-        printf("Digite a matricula do aluno: "); //PROCURAR POR matricula
-        scanf("%d", &matricula);
+    printf("Digite a matricula do aluno: "); //PROCURAR POR matricula
+    scanf("%d", &mat);
+    
+    while(aux->matricula != mat){
+        aux = aux->prox;
+    }
 
-    if(tam == 1 && inicio->matricula == matricula){
-        BANCA *lixo = inicio;
-        inicio = NULL;
-        inicio = NULL;
-        free(lixo);
-        tam--;
-    }else{
-        BANCA * aux = inicio;
-        while(aux != fim && aux->matricula != matricula){
-            aux = aux->prox;
+    printf("1-Aluno: %s\n", aux->aluno);
+    printf("2-Matricula: %d\n", aux->matricula);
+    printf("3-Curso: %s\n", aux->curso);
+    printf("4-Email do aluno: %s\n", aux->email_aluno);
+    printf("5-Orientador: %s\n", aux->orientador);
+    printf("6-Frequencia: %lf\n", aux->frequencia);
+    printf("7-Avaliadores: %s\n", aux->avaliadores);
+    printf("8-Instituição: %s\n", aux->instituicao);
+    printf("9-E-mail para contato: %s\n", aux->email_avaliador);
+    printf("10-Título: %s\n", aux->titulo_banca);
+    printf("11-Resumo:  %s\n", aux->resumo);
+    printf("12-Horário: %lf\n", aux->horario);
+    printf("13-Local: %s\n", aux->local); 
+
+    printf("Digite o numero referente ao campo que voce quer alterar: "); 
+    scanf("%d", &opcao);
+
+    switch(opcao){
+            case 1: 
+                printf("Digite o nome do aluno: ");
+                scanf("%s", aluno);
+                aux->aluno = aluno; 
+                break; 
+            case 2: 
+                printf("\nDigite a matricula do aluno:");
+                scanf("%d", &matricula);
+                aux->matricula = matricula; 
+                break; 
+            case 3: 
+                printf("\nDigite o curso do aluno: ");
+                scanf("%s", curso);
+                aux->curso = curso; 
+                break; 
+            case 4: 
+                printf("\nDigite o e-mail do aluno: ");
+                scanf("%s", email_aluno);
+                aux->email_aluno = email_aluno; 
+                break; 
+            case 5: 
+                printf("\nDigite o nome do orientador: ");
+                scanf("%s", orientador);
+                aux->orientador = orientador; 
+                break; 
+            case 6: 
+                printf("\nDigite frequencia: ");
+                scanf("%lf", &frequencia);
+                aux->frequencia = frequencia; 
+                break; 
+            case 7: 
+                printf("\nDigite o nome dos avaliadores: ");
+                scanf("%s", avaliadores);
+                aux->avaliadores = avaliadores; 
+                break; 
+            case 8: 
+                printf("\nDigite o e-mail para contato: ");
+                scanf("%s", instituicao);
+                aux->instituicao = instituicao; 
+                break; 
+            case 9: 
+                printf("\nDigite o e-mail para contato: ");
+                scanf("%s", email_avaliador);
+                aux->email_avaliador = email_avaliador; 
+                break; 
+            case 10: 
+                printf("\nDigite o título da banca: ");
+                scanf("%s", titulo_banca);
+                aux->titulo_banca = titulo_banca; 
+                break; 
+            case 11: 
+                printf("\nDigite o resumo: ");
+                scanf("%s", resumo);
+                aux->resumo = resumo; 
+                break; 
+            case 12: 
+                printf("\nDigite o horario: ");
+                scanf("%lf", &horario);
+                aux->horario = horario; 
+                break; 
+            case 13: 
+                printf("\nDigite o local: ");
+                scanf("%s", local);
+                aux->local = local; 
+                break;   
         }
-        if(aux->matricula == matricula){
-            aux->ant->prox = aux->prox;
-            aux->prox->ant = aux->ant;
-            if(inicio->matricula == matricula){
-                inicio = inicio->prox;
-            }
-            if(fim->matricula == matricula){
-                fim = fim->prox;
-            }
-            free(aux);
-            tam--;
-        }else{
-            printf("Não foi encontrado! \n");
-        }
-   }
+    printf("Banca atualizada com sucesso!");
 }
-
 
 int main(){  
     
-    //------------------------------criando menu de opcoes CRUD---------------------------------------
-    int x = 1;
+  int x = 1;
     while(x==1){
         int menu;
         printf("\n---------------------------MENU | CRUD---------------------------\n");
